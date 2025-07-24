d3c0e08bd6b930adc1c744762b136b250580a1b8
Bryan Cabello <bjcabello55@gmail.com>
2025-07-24 05:51:06 -0500

    modificando config.py

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,9 +1,13 @@
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent
-BASE_CONTEXT = BASE_DIR / "context" / "base"
-PROCESSES_CONTEXT = BASE_DIR / "context" / "processes"
-DOCUMENTS_CONTEXT = BASE_DIR / "context" / "documents"
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+BASE_CONTEXT = BASE_DIR / "src" / "context" / "base"
+PROCESSES_CONTEXT = BASE_DIR / "src" / "context" / "processes"
+DOCUMENTS_CONTEXT = BASE_DIR / "src" / "context" / "documents"
+
 
 OLLAMA_URL = "http://localhost:11434/api/chat"
 CHROMA_COLLECTION = "procesos_indexados"
