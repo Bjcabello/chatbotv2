1ac58df4508ece805e80392096e959b190706eec
Bryan Cabello <bjcabello55@gmail.com>
2025-08-01 11:26:16 -0500

    modificando frontend

--- frontend/src/components/ChatBox.jsx
+++ frontend/src/components/ChatBox.jsx
@@ -84,7 +84,7 @@ function ChatBox() {
 
   return (
     <div className="container-fluid bg-light mt-4 border border-3" style={{ maxWidth: '900px' }}>
-      <h3 className="text-center">ChatBot 🤖 Viadocs</h3>
+      <h3 className="text-center">ChatBot  Viadocs</h3>
       <div className="row justify-content-center mt-5">
         <div className="col-12 col-md-4 mb-3 d-flex flex-column align-items-center">
           <label className="form-label fw-bold" htmlFor="usuario">
@@ -136,20 +136,20 @@ function ChatBox() {
 
         <div className="col-12 col-md-4 mb-3 d-flex flex-column align-items-center">
           <label className="form-label fw-bold" htmlFor="archivoPDF">
-            Subir PDF:
+            Adjunta PDF:
           </label>
           <input
             type="file"
             id="archivoPDF"
             accept=".pdf"
             className="form-control"
-            style={{ maxWidth: '200px' }}
+            style={{ Width: '250px' }}
             onChange={(e) => setArchivoPDF(e.target.files[0])}
             aria-label="Subir archivo PDF"
           />
           <button
             className="btn btn-outline-success mt-2"
-            style={{ maxWidth: '200px' }}
+            style={{ width: '200px' }}
             onClick={handleSubirPDF}
           >
             Subir PDF
