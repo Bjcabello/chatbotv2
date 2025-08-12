9c8db04bfb66f972bf1840d0902c39c0b36a5a55
Alexis Cepeda <alexkiller0408@hotmail.com>
2025-08-12 01:28:07 -0500

    filtrado de respuestas por array

--- backend/src/routes/chat.py
+++ backend/src/routes/chat.py
@@ -6,7 +6,7 @@ from src.utils.chroma import indexar_documento
 from pathlib import Path
 import shutil
 import os
-
+from sentence_transformers import SentenceTransformer, util
 router = APIRouter()
 
 @router.post("/upload-pdf")
@@ -42,6 +42,66 @@ def procesar_pdf_en_background(ruta: str, nombre_archivo: str):
             os.remove(ruta)
 
 
+# Cargamos un modelo de embeddings ligero para clasificación
+classifier_model = SentenceTransformer("all-MiniLM-L6-v2")
+
+pdf_examples = [
+    "¿Qué dice el documento?",
+    "Explícame el archivo que subí",
+    "Según el PDF que envié",
+    "Resume la página 2 del documento",
+    "Analiza el texto del archivo",
+    "pdf",
+    "explicame",
+    "archivo",
+    "pdf",
+    "quiere decir",
+    "documento",
+    "papel",
+    "dime",
+    "entender",
+    "que",
+    "como",
+    "cuantos",
+    "cuanto",
+    "donde",
+    "quien"
+    "quienes"
+]
+
+md_examples = [
+    "¿Cuál es la lógica de negocio?",
+    "¿Qué restricciones hay?",
+    "Describe la personalidad del sistema",
+    "Explica las reglas del sistema",
+    "Contexto base",
+    "eliminar",
+    "como creo un usuario?",
+    "como edito un usuario?",
+    "como borro un usuario?",
+    "crear",
+    "editar",
+    "viamatica",
+    "instrucciones"
+    "gracias"
+    "adios"
+]
+
+def detectar_tipo_pregunta(pregunta: str) -> str:
+    embedding_pregunta = classifier_model.encode(pregunta, convert_to_tensor=True)
+
+    # Similaridad con ejemplos PDF
+    embedding_pdf = classifier_model.encode(pdf_examples, convert_to_tensor=True)
+    score_pdf = util.cos_sim(embedding_pregunta, embedding_pdf).max().item()
+
+    # Similaridad con ejemplos Markdown
+    embedding_md = classifier_model.encode(md_examples, convert_to_tensor=True)
+    score_md = util.cos_sim(embedding_pregunta, embedding_md).max().item()
+
+    print(f"📊 Similitud PDF: {score_pdf:.4f} | Markdown: {score_md:.4f}")
+
+    return CHROMA_COLLECTION_PDF if score_pdf > score_md else CHROMA_COLLECTION_MD
+
 @router.post("/chat")
 def chat(data: Chat):
     try:
@@ -51,12 +111,11 @@ def chat(data: Chat):
 
         llm = OllamaLLM(model="llama3", temperature=0)
 
-         # 🔍 Aquí decides la colección según la pregunta
-        if "pdf" in data.pregunta.lower() or "documento" in data.pregunta.lower():
-            collection_name = CHROMA_COLLECTION_PDF
-        else:
-            collection_name = CHROMA_COLLECTION_MD
+        #  Aquí decides la colección según la pregunta
+        # 📌 Usamos la detección semántica
+        collection_name = detectar_tipo_pregunta(data.pregunta)
 
+        
 
         from src.utils.chroma import get_chroma_vectorstore
         retriever = get_chroma_vectorstore(collection_name).as_retriever(search_kwargs={"k": 5})
