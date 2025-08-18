15bb2b4fcc6d758b0acdce025cb51ac80a7a58b8
Bryan Cabello <bjcabello55@gmail.com>
2025-08-18 06:00:42 -0500

    agregando route de registro login y chatbox

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Register from './components/Register';
-import Login from './components/Login';
-import ChatBox from './components/ChatBox';
+import Register from './components/Register.jsx';
+import Login from './components/Login.jsx';
+import ChatBox from './components/ChatBox.jsx';
 
 function App() {
   return (
