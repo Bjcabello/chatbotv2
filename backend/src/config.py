34530203ed5e3222c43d81fb8a14ff2f12941e20
Alexis Cepeda <alexkiller0408@hotmail.com>
2025-08-27 22:44:26 -0500

    lee bien los pdfs

--- backend/src/config.py
+++ backend/src/config.py
@@ -17,6 +17,6 @@ CHROMA_COLLECTION_PDF = "documentos_pdf"
 CHROMA_COLLECTION_MD = "documentos_md"
 
 
-LIMIT_PAGE_PDF = 3
+LIMIT_PAGE_PDF = 30
 CHROMA_MARKDOWN_COLLECTION = "markdown_index"  
 CHROMA_PDF_COLLECTION = "pdf_index"  
