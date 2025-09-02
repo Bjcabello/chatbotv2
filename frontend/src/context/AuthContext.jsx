92bf47176b4f8cd5089e53e3142f2490e7538496
theNunu <152096019+theNunu@users.noreply.github.com>
2025-09-02 11:57:05 -0500

    la autenticacion tira false (frontend)

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -3,12 +3,17 @@ import React, { createContext, useState, useEffect } from 'react';
 export const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('token'));
+  const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('token',
+    !!localStorage.getItem('api_key')
+
+  ));
   // const [userId, setUserId] = useState(localStorage.getItem('userId') || null);
 
-  const login = (token) => {
+  const login = (token, apiKey, apiKeyExpiresAt) => {
     localStorage.setItem('token', token);
     // localStorage.setItem('userId', userId);
+    localStorage.setItem('apiKey', apiKey);
+    localStorage.setItem('expires_at', apiKeyExpiresAt)
     setIsAuthenticated(true);
     // setUserId(userId);
   };
@@ -16,6 +21,8 @@ export const AuthProvider = ({ children }) => {
   const logout = () => {
     localStorage.removeItem('token');
     // localStorage.removeItem('userId');
+    localStorage.removeItem('apiKey');
+    localStorage.removeItem('expires_at')
     setIsAuthenticated(false);
     // setUserId(null);
   };
@@ -23,9 +30,17 @@ export const AuthProvider = ({ children }) => {
   useEffect(() => {
     const checkToken = () => {
       const token = localStorage.getItem('token');
-      if (token) {
+      const apiKey = localStorage.getItem('api_key');
+      const apiKeyExpiresAt = localStorage.getItem('expires_at');
+      if (token && apiKey) {
         // Aquí podrías usar jwt-decode para verificar expiración
-        setIsAuthenticated(true);
+        const expiresAt = new Date(apiKeyExpiresAt);
+        if (expiresAt < new Date()) {
+          console.log('AuthContext - API Key expired, logging out');
+          logout();
+        } else {
+          setIsAuthenticated(true);
+        }
       } else {
         setIsAuthenticated(false);
       }
@@ -34,7 +49,7 @@ export const AuthProvider = ({ children }) => {
   }, []);
 
   return (
-    <AuthContext.Provider value={{ isAuthenticated, login, logout}}>
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
       {children}
     </AuthContext.Provider>
   );
